f676102f334c590fabb53120d6a6b90b803b1199
Andrey Spies <andreyspies@gmail.com>
2022-03-03 21:08:33 -0300

    Add miragejs

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -19,7 +19,7 @@ export function Summary() {
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Saídas" />
                 </header>
-                <strong>- 5000,00</strong>
+                <strong>- 500,00</strong>
             </div>
             <div className='highlight-background'>
                 <header>
